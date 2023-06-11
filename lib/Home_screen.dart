c4dc44765ad8b8a5a02ea3ebd5e6d08fe894fcb0
Mir-Tamzid <mir.hasan@graduate.utm.my>
2023-06-12 04:46:29 +0800

    Home updated

---
+++ lib/Home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_app/consts/consts.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
