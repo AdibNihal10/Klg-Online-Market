5f17fe1ec3c1fd6716732d5efe7bc622e6c22ff8
AdibNihal10 <adibnihal10@gmail.com>
2023-06-12 02:10:42 +0800

    first commit

---
+++ lib/consts/strings.dart
@@ -0,0 +1,13 @@
+const appname = "eMart";
+const appversion = "Version 1.0.0";
+const credits = "@Baaba Devs";
+const email = "Email";
+const emailHint = "Enter mail";
+const password = "Password";
+const passwordHint = "*******";
+const forgetPass = "Forget Password";
+const login = "Log in";
+const signup = "Sign up";
+
+const createNewAccount = "or, create a New Account";
+const loginwith = "Log in with";
