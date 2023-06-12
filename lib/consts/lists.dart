e3e266e4a3dc4fcbb92b36a2ed72564f0f61fc6a
ruzhan05 <ruzhan05@gmail.com>
2023-06-12 17:30:06 +0800

    item details page added

---
+++ lib/consts/lists.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_app/consts/consts.dart';
+import 'package:flutter_app/views/category_screen/item_details.dart';
+
+const socialIconList = [icFacebookLogo, icGoogleLogo, icTwitterLogo];
+
+const slidersList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
+const secondSlidersList = [imgSs1, imgSs2, imgSs3, imgSs4];
+
+const categoryImages = [
+  imgS1,
+  imgFc1,
+  imgFc2,
+  imgFc3,
+  imgFc4,
+  imgFc5,
+  imgS11,
+  imgFc6,
+  imgFc9,
+];
+
+const itemDetailButtonsList = [
+  video,
+  reviews,
+  privacyPolicy,
+  returnpolicy,
+  supportPolicy
+];
